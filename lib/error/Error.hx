15222ddd9188f593ac4b8e03191e8d00fd388226
snsvrno <snsvrno@tuta.io>
2023-06-06 13:26:48 -0600

    refactor: updated to simpler interface
    changes the idea from using a hash map to make new error classes for
    every kind of error that we may want, and have that class implement how
    te handle the message.

--- lib/error/Error.hx
+++ lib/error/Error.hx
@@ -1,40 +1,5 @@
 package error;
 
-class Error {
-	public static var formats : Map<String,String> = Macros.buildMessageTemplates();
-	public static var functions : Map<String, (s:String) -> String> = [
-	#if ansi
-		"red" => (s:String) -> ansi.Paint.paint(s, Red),
-		"green" => (s:String) -> ansi.Paint.paint(s, Green),
-		"yellow" => (s:String) -> ansi.Paint.paint(s, Yellow),
-		"blue" => (s:String) -> ansi.Paint.paint(s, Blue),
-		"magenta" => (s:String) -> ansi.Paint.paint(s, Magenta),
-		"cyan" => (s:String) -> ansi.Paint.paint(s, Cyan),
-	#end
-	];
-
-
-	public var kind : String;
-	public var values : Map<String,Dynamic> = [];
-
-	public function new(details : Dynamic) {
-		var keys = Reflect.fields(details);
-		for (k in keys) {
-			if (k == "kind") kind = Reflect.getProperty(details, k);
-			else values.set(k, Reflect.getProperty(details, k));
-		}
-	}
-
-	public function toString() : String {
-		var format = formats.get(kind);
-
-		if (format == null) {
-			var msg = 'ERROR kind: $kind';
-			for (k => v in values) msg += ', $k: $v';
-			return msg;
-		} else {
-			return Format.format(format, this);
-		}
-	}
-
+interface Error {
+	public function msg() : String;
 }
--- lib/error/ErrorTools.hx
+++
@@ -1,9 +0,0 @@
-package error;
-
-class ErrorTools {
-
-	inline public static function add(e : Error, values : Dynamic) {
-		for (k in Reflect.fields(values))
-			msg.values.set(k, Reflect.getProperty(values, k));
-	}
-}
